881b23b88c7302dc04efce6d285994e9b4e0e0c0
evansavage <esav678@gmail.com>
2025-01-14 14:49:31 -0500

    New version of 30

--- permutations-30.js
+++ permutations-30.js
@@ -1,9 +1,12 @@
 // permutations 30
 
+speed=0.8
+
 noise(5)
 .colorama(.1)
 .modulate(noise(.001, [0.001, 0.0015].smooth()),0.25)
+.modulateScale(osc(1, 0.5, 0.2))
 .diff(src(o0).rotate(10, .3).mult(shape(4), 2), 100)
 .modulateScrollX(osc(.8,2,4))
-.scale(1.05)
+.scale(0.9)
 .out()
